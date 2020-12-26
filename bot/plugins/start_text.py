f0c094ef899bdd8f41d388f55929fb79a6d92b77
AnonymousSurat <69172674+AnonymousSurat@users.noreply.github.com>
2020-12-27 01:57:41 +0530

    Update start_text.py

--- bot/plugins/start_text.py
+++ bot/plugins/start_text.py
@@ -38,7 +38,7 @@ from bot.hf.flifi import uszkhvis_chats_ahndler
 )
 async def num_start_message(_, message: Message):
     await message.reply_text(
-        f"Hi {message.from_user.first_name}START_OTHER_USERS_TEXT\n\n",
+        f"Hi {message.from_user.first_name}\n\n" START_OTHER_USERS_TEXT,
         quote=True
     ) 
 
